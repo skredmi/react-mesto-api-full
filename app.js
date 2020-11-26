4e40d0056f0888ec3cc81c39fb51fb988a10025c
Dmitriy Skrebkov <skredmi91@gmail.com>
2020-11-26 22:57:06 +0300

    commit

--- app.js
+++ app.js
@@ -14,6 +14,12 @@ const app = express();
 const { PORT = 3000 } = process.env;
 app.use(cors());
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
