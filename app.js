d8c72048a3a9001bccfe0e0e31c8b05c4bafcbbd
Dmitriy Skrebkov <skredmi91@gmail.com>
2020-11-27 14:00:32 +0300

    test 8

--- app.js
+++ app.js
@@ -12,13 +12,14 @@ const { requestLogger, errorLogger } = require('./middlewares/logger.js');
 
 const app = express();
 const { PORT = 3000 } = process.env;
+
 app.use(cors());
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+//   next();
+// });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
