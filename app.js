24e29efeaad06009d3f566792bae3f4af45439ed
Dmitriy Skrebkov <skredmi91@gmail.com>
2020-11-27 14:13:51 +0300

    test 9

--- app.js
+++ app.js
@@ -15,11 +15,11 @@ const { PORT = 3000 } = process.env;
 
 app.use(cors());
 
-// app.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', '*');
-//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-//   next();
-// });
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
