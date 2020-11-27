ff32db852c4bfc27cb3f29270fb01995c1c4902e
Dmitriy Skrebkov <skredmi91@gmail.com>
2020-11-27 13:49:30 +0300

    test 7

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const { PORT = 3000 } = process.env;
 app.use(cors());
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin' : '*');
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
@@ -46,9 +46,9 @@ app.post('/signin', celebrate({
 }), login);
 app.post('/signup', celebrate({
   body: Joi.object().keys({
-    // name: Joi.string().min(2).max(30),
-    // about: Joi.string().min(2).max(30),
-    // avatar: Joi.string().regex(/^http[s]?:\/\/\w+/),
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string().regex(/^http[s]?:\/\/\w+/),
     email: Joi.string().required().email(),
     password: Joi.string().required().min(6),
   }),
