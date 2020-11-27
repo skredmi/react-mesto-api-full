4c80ce8c3e4bd972defda415502cf4d384b6c3ed
Dmitriy Skrebkov <skredmi91@gmail.com>
2020-11-27 13:11:55 +0300

    test 5

--- app.js
+++ app.js
@@ -46,9 +46,9 @@ app.post('/signin', celebrate({
 }), login);
 app.post('/signup', celebrate({
   body: Joi.object().keys({
-    name: Joi.string().min(2).max(30),
-    about: Joi.string().min(2).max(30),
-    avatar: Joi.string().regex(/^http[s]?:\/\/\w+/),
+    // name: Joi.string().min(2).max(30),
+    // about: Joi.string().min(2).max(30),
+    // avatar: Joi.string().regex(/^http[s]?:\/\/\w+/),
     email: Joi.string().required().email(),
     password: Joi.string().required().min(6),
   }),
